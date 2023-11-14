1252631cbbecff67e7d5713d97b29ecae0db09a9
WZFFMK <113918773+WZFFMK@users.noreply.github.com>
2023-11-14 11:19:16 +0800

    Add files via upload

---
+++ PUBG.sh
@@ -0,0 +1,18 @@
+rm -rf nohup.out
+cp lib/temp /data/tempp
+cp lib/temppubg /data/tempp2
+chmod 777 /data/tempp*
+# 获取 Android 版本号
+android_version=$(getprop ro.build.version.release)
+
+settings put global block_untrusted_touches 0
+
+# 判断版本号是否以 "9" 开头
+if [[ "$android_version" == 9* ]]; then
+    /data/tempp
+else
+    rm -rf nohup.out
+nohup /data/tempp
+cat nohup.out
+fi
+
